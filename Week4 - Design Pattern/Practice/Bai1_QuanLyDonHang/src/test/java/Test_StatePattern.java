1a0b3f6931829aaac2e7513dce57be22130adfe9
nguyen kha <nkha3561@gmail.com>
2025-03-31 13:40:31 +0700

    add week 6 docker - phan 1

--- Week4 - Design Pattern/Practice/Bai1_QuanLyDonHang/src/test/java/Test_StatePattern.java
+++ Week4 - Design Pattern/Practice/Bai1_QuanLyDonHang/src/test/java/Test_StatePattern.java
@@ -1,11 +1,27 @@
+import org.example.State.CancelledState;
 import org.example.State.DeliveredState;
 import org.example.State.OrderContext;
 
 public class Test_StatePattern {
     public static void main(String[] args) {
-        OrderContext orderContext = new OrderContext();
-        orderContext.processOrder(); // NewOrderState -> ProcessingState
-        orderContext.processOrder(); // ProcessingState -> DeliveredState
-        orderContext.processOrder(); // DeliveredState
+        OrderContext orderContext1 = new OrderContext();
+
+        System.out.println("New - processing - deliveried Order State case:");
+        orderContext1.processOrder(); // NewOrderState -> ProcessingState
+        orderContext1.processOrder(); // ProcessingState -> DeliveredState
+        orderContext1.processOrder(); // DeliveredState
+
+
+        OrderContext orderContext2 = new OrderContext();
+        System.out.println("\nCancel order case");
+        orderContext2.setState(new CancelledState());
+        orderContext2.processOrder(); // CancelledState
+
+        OrderContext orderContext3 = new OrderContext();
+        System.out.println("\nProcessing - cancel order case");
+        orderContext3.processOrder(); // NewOrderState -> ProcessingState
+        orderContext3.setState(new CancelledState());
+        orderContext3.processOrder(); // CancelledState
+
     }
 }
